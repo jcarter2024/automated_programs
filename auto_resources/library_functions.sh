1566b2e7ae8a79508b30c0b0780e223f6b48ca1f
Joshua carter <joshuacarter@joshuas-mbp.mynetworksettings.com>
2024-06-06 17:18:20 -0400

    Improved routines to allow multiple library versions

--- auto_resources/library_functions.sh
+++ auto_resources/library_functions.sh
@@ -3,12 +3,56 @@
 #To do
 #        --> This script contains library building functions
 
+########################### VERSIONS ##########################
+# ------ ZLIB
+zlib_url=https://zlib.net/fossils/zlib-1.2.11.tar.gz
+zlib_url=https://zlib.net/zlib-1.3.1.tar.gz
+
+# ------ libpng
+libpng_url=https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.xz
+libpng_url=https://sourceforge.net/projects/libpng/files/libpng16/1.6.43/libpng-1.6.43.tar.xz
+
+# ------ hdf5
+hdf5_url=https://hdf-wordpress-1.s3.amazonaws.com/wp-content/uploads/manual/HDF5/HDF5_1_12_0/source/hdf5-1.12.0.tar.gz
+hdf5_url=https://github.com/HDFGroup/hdf5/releases/download/hdf5_1.14.4.3/hdf5-1.14.4-3.tar.gz
+
+# ------ netcdf
+netcdf_url=https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz
+
+# ------ netcdf_Fortran
+netcdfF_url=https://downloads.unidata.ucar.edu/netcdf-fortran/4.6.1/netcdf-fortran-4.6.1.tar.gz
+
+# ------ jasper
+jasper_url=https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.29.tar.gz
+jasper_url=https://github.com/jasper-software/jasper/archive/refs/tags/version-4.2.4.tar.gz
+
+
+# ------- strip the version to install correctly
+zlib_tar="${zlib_url##*/}"
+zlib_v=(${zlib_tar//".tar"/ })
+
+libpng_tar="${libpng_url##*/}"
+libpng_v=(${libpng_tar//".tar"/ })
+
+hdf5_tar="${hdf5_url##*/}"
+hdf5_v=(${hdf5_tar//".tar"/ })
+
+netcdf_tar="${netcdf_url##*/}"
+netcdf_v=(${netcdf_tar//".tar"/ })
+
+netcdfF_tar="${netcdfF_url##*/}"
+netcdfF_v=(${netcdfF_tar//".tar"/ })
+
+jasper_tar="${jasper_url##*/}"
+jasper_v=(${jasper_tar//".tar"/ })
+
+
 ####### FUNCTIONS ############################
 
 build_zlib() {
-    wget https://zlib.net/fossils/zlib-1.2.11.tar.gz
-    tar xvf zlib-1.2.11.tar.gz >& zlibtar.log
-    cd zlib-1.2.11/
+    wget $zlib_url
+    tar xvf $zlib_tar >& zlibtar.log
+    cd $zlib_v
     ./configure --prefix=$1 >& zlibconfig.txt
     echo "Making zlib..."
     make >& zlibmake.log
@@ -20,61 +64,62 @@ build_zlib() {
     else
         echo "cleaning up"
         cd ../
-        rm zlib-1.2.11.tar.gz
-        rm -r zlib-1.2.11
+        rm $zlib_tar
+        rm -r $zlib_v
         rm zlibtar.log
     fi
 }
+#build_zlib "/Users/joshuacarter/Desktop/bashtestingdelete/intel_test/"
 
-
-build_libpng () {
-  wget https://downloads.sourceforge.net/project/libpng/libpng16/1.6.37/libpng-1.6.37.tar.xz
-    xz -d -v libpng-1.6.37.tar.xz >& libpngxz.log
-    tar xvf libpng-1.6.37.tar >& libpngtar.log
-    cd libpng-1.6.37/
-    ./configure --prefix="$1" >& libpngconfig.txt
+build_libpng() {
+    wget $libpng_url
+    tar xvf $libpng_tar >& libpngtar.log
+    cd $libpng_v
+    ./configure --prefix=$1 >& libpngconfig.txt
     echo "Making libpng..."
     make >& libpngmake.log
     make install >& libpnginstall.log
-    if [ ! -f $1include/libpng16/pnglibconf.h ]; then
+    #check it worked
+    if [ ! -f $1include/pngconf.h ]; then
         echo "libpng build error"
         kill -INT $$
     else
-        cd ../
         echo "cleaning up"
-        rm libpng-1.6.37.tar.xz
-        rm libpng-1.6.37.tar
-        rm -r libpng-1.6.37
+        cd ../
+        rm $libpng_tar
+        rm -r $libpng_v
         rm libpngtar.log
-        rm libpngxz.log
     fi
 }
+#build_libpng "/Users/joshuacarter/Desktop/bashtestingdelete/intel_test/"
 
-build_hdf5 () {
-    wget https://hdf-wordpress-1.s3.amazonaws.com/wp-content/uploads/manual/HDF5/HDF5_1_12_0/source/hdf5-1.12.0.tar.gz 
-    tar xvf hdf5-1.12.0.tar.gz >& hdf5tar.log
-    cd hdf5-1.12.0
+build_hdf5() {
+    wget $hdf5_url
+    tar xvf $hdf5_tar >& hdf5tar.log
+    cd $hdf5_v
     ./configure --prefix=$1 --with-zlib=$1 --enable-fortran >& hdf5config.txt
     echo "Making HDF5..."
     make >& hdf5make.log
     make install >& hdf5install.log
     if [ ! -f $1include/H5FDcore.h ]; then
-    echo "libpng build error"
+        echo "hdf5 build error"
         kill -INT $$
     else
-        cd ../
         echo "cleaning up"
-        rm hdf5-1.12.0.tar.gz
-        rm -r hdf5-1.12.0
+        cd ../
+        rm $hdf5_tar
+        rm -r $hdf5_v
         rm hdf5tar.log
     fi
 }
+#build_hdf5 "/Users/joshuacarter/Desktop/bashtestingdelete/intel_test/"
 
 build_netcdf () {
-    #both netcdf and netcdf fortran are required
-    wget https://downloads.unidata.ucar.edu/netcdf-c/4.9.2/netcdf-c-4.9.2.tar.gz
-    tar xvf netcdf-c-4.9.2.tar.gz >& netcdftar.log
-    cd netcdf-c-4.9.2
+    wget $netcdf_url
+    tar xvf $netcdf_tar >& netcdftar.log
+    echo $netcdf_v
+    cd $netcdf_v
+    pwd
     export LD_LIBRARY_PATH="$1lib:$LD_LIBRARY_PATH"
     export LDFLAGS="-L$1lib"
     export CPPFLAGS="-I$1include"
@@ -82,55 +127,76 @@ build_netcdf () {
     echo "Making NETCDF..."
     make >& netcdfmake.log
     make install >& netcdfinstall.log
-    if [ ! -f $1include/zlib.h ]; then
-    echo "libpng build error"
+    if [ ! -f $1include/netcdf.h ]; then
+    echo "netcdf build error"
         kill -INT $$
     else
         cd ../
         echo "cleaning up"
-        rm netcdf-c-4.9.2.tar.gz
-        rm -r netcdf-c-4.9.2
+        rm $netcdf_tar
+        rm -r $netcdf_v
         rm netcdftar.log
     fi
 
-    wget https://downloads.unidata.ucar.edu/netcdf-fortran/4.6.1/netcdf-fortran-4.6.1.tar.gz
-    tar xvf netcdf-fortran-4.6.1.tar.gz >& netcdfFtar.log
-    cd netcdf-fortran-4.6.1  
+    wget $netcdfF_url
+    tar xvf $netcdfF_tar >& netcdfFtar.log
+    cd $netcdfF_v  
     ./configure --prefix=$1  >& netcdfFconfig.txt
     echo "Making NETCDF (Fortran)..."
     make >& netcdfFmake.log
     make install >& netcdfFinstall.log
-    if [ ! -f $1include/zlib.h ]; then
-    echo "libpng build error"
+    if [ ! -f $1include/netcdf_nf_interfaces.mod ]; then
+    echo "netcdf Fortran build error"
         kill -INT $$
     else
         cd ../
         echo "cleaning up"
-        rm netcdf-fortran-4.6.1.tar.gz
-        rm -r netcdf-fortran-4.6.1
+        rm $netcdfF_tar
+        rm -r $netcdfF_v
         rm netcdfFtar.log
-        rm netcdfFmake.log
-        rm netcdfFinstall.log
-        rm netcdfFconfig.txt
+    fi
+}
+
+cmake_jasper () {
+    wget $jasper_url
+    tar xvf $jasper_tar >& jaspertar.log
+    #create a temporary build directory because cmake things
+    mkdir BUILD
+    BUILD_DIR=$(pwd)/BUILD
+    echo $BUILD_DIR
+    cd "jasper-$jasper_v"
+    cmake  -B$BUILD_DIR -DCMAKE_INSTALL_PREFIX=$1
+    cmake --build $BUILD_DIR
+    cmake --build $BUILD_DIR --target install
+    if [ ! -f $1include/jasper/jasper.h ]; then
+    echo "jasper build error"
+        kill -INT $$
+    else
+        cd ../
+        echo "cleaning up"
+        rm $jasper_tar
+        rm -r "jasper-$jasper_v"
+        rm -r BUILD
+        rm jaspertar.log
     fi
 }
 
 build_jasper () {
-    wget https://www.ece.uvic.ca/~frodo/jasper/software/jasper-1.900.29.tar.gz
-    tar xvf jasper-1.900.29.tar.gz >& jaspertar.log
-    cd jasper-1.900.29/
+    wget $jasper_url
+    tar xvf $jasper_tar >& jaspertar.log
+    cd "jasper-$jasper_v"
     ./configure --prefix="$1" >& jasperconfig.txt
     echo "Making jasper..."
     make >& jaspermake.log
     make install >& jasperinstall.log
-    if [ ! -f $1include/zlib.h ]; then
-    echo "libpng build error"
+    if [ ! -f $1include/netscdf.h ]; then
+    echo "jasper build error"
         kill -INT $$
     else
         cd ../
         echo "cleaning up"
-        rm jasper-1.900.29.tar.gz
-        rm -r jasper-1.900.29
+        rm $jasper_tar
+        rm -r "jasper-$jasper_v"
         rm jaspertar.log
     fi
 }
